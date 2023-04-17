765fa16b42455410f2d0d48f7849e8b748df0292
gallojunior <gallojunior@gmail.com>
2023-04-17 08:19:14 -0300

    Project Created

---
+++ ComicsUniverse/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ComicsUniverse.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
